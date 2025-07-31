6c6166a1c157217eec790daa05a8de3389cd037c
francescoluca <francescoluca14@gmail.com>
2025-07-31 16:24:37 +0200

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/chess-game/', // Aggiungi questa riga
+  base: '/chess-game/',
   server: {
     port: 8080,
     open: true
